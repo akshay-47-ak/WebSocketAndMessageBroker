cf82e647df9c59c5e0b6b8fd8a9c00eb299721cc
akshay-47-ak <akshayathavale1525@gmail.com>
2026-02-19 21:33:29 +0530

    Learning WebSocket And Message Broker

---
+++ src/main/java/com/websocketut/NotificationMB/NotificationMbApplication.java
@@ -0,0 +1,13 @@
+package com.websocketut.NotificationMB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationMbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocketut/NotificationMB/NotificationMbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketut.NotificationMB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
